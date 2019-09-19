810a46054b284a368cd530da9870f7b41a1129f4
Alan Shih <alan0201tw@gmail.com>
2019-09-20 00:25:37 +0800

    0920init

--- src/vm.c
+++ src/vm.c
@@ -1,5 +1,7 @@
 #include "vm.h"
 
+#include "debug.h"
+
 // a damned global variable
 VM vm;
 
@@ -16,12 +18,32 @@ void freeVM()
 static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
     for(;;)
     {
+#ifdef DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+
         uint8_t instruction;
         switch (instruction = READ_BYTE())
         {
+        case OP_CONSTANT_LONG:
+        {
+            uint32_t index = READ_BYTE();
+            index |= READ_BYTE() << 8;
+            index |= READ_BYTE() << 16;
+            push(vm.chunk->constants.values[index]);
+            break;
+        }
+        case OP_CONSTANT:
+        {
+            Value constant = READ_CONSTANT();
+            printValue(constant);
+            printf("\n");
+            break;
+        }
         case OP_RETURN:
             return INTERPRET_OK;
 
@@ -30,7 +52,8 @@ static InterpretResult run()
         }
     }
 
-#undef READ_BYTE()
+#undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk* chunk)
