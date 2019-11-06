047ebf8504da89d314ffb77872c0be7e05f2b57a
DESKTOP-6J14KSJ\AlanShih <alan0201tw@gmail.com>
2019-11-07 00:07:33 +0800

    Toward  Identifiers and Keywords

--- src/compiler.c
+++ src/compiler.c
@@ -7,4 +7,33 @@
 void compile(const char* source)
 {
     initScanner(source);
+
+    int line = -1;
+    for(;;)
+    {
+        Token token = scanToken();
+        if(token.line != line)
+        {
+            printf("%4d ", token.line);
+            line = token.line;
+        }
+        else
+        {
+            printf("   | ");
+        }
+        /**
+         *  Reference : http://www.craftinginterpreters.com/scanning-on-demand.html#a-token-at-a-time
+         * 
+         *  That %.*s in the format string is a neat feature. 
+         *  Usually, you set the output precision — the number of characters to show — by placing a number 
+         *  inside the format string. Using * instead lets you pass the precision as an argument. 
+         *  So that printf() call prints the first token.length characters of the string at token.start. 
+         *  We need to limit the length like that because the lexeme points into the original source 
+         *  string and doesn’t have a terminator at the end.
+         */
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if(token.type == TOKEN_EOF)
+            break;
+    }
 }
