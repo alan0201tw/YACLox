6c91b986438fa035310f41dfe307e6c23dfd2f20
alan0201tw <alan0201tw@gmail.com>
2019-12-20 23:00:14 +0800

    init hash table implementation

---
+++ include/table.h
@@ -0,0 +1,25 @@
+#ifndef clox_table_h
+#define clox_table_h
+
+#include "common.h"
+#include "value.h"
+
+// The ratio of count to capacity is the load factor of the hash table.
+
+typedef struct
+{
+    ObjString* key;
+    Value value;
+} Entry;
+
+typedef struct
+{
+    int count;
+    int capacity;
+    Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+
+#endif
